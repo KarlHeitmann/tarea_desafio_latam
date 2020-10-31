61505cb428c1ee16fa6ba91be23ba16198b2eb37
Karl Heitmann <khf1988@gmail.com>
2020-10-31 18:16:48 -0300

    agregado helper build_web_page

--- app/helpers/nasa_photos_helper.rb
+++ app/helpers/nasa_photos_helper.rb
@@ -1,2 +1,19 @@
 module NasaPhotosHelper
+=begin
+  def format_name(user)
+    if user.gender == "M"
+      "Mr. #{user.name}"
+    else
+      "Ms. #{user.name}"
+    end
+  end
+=end
+  def build_web_page(photos)
+    output = ""
+    photos.each do |photo|
+      output += photo['img_src'] + '\n'
+    end
+    output
+  end
+
 end
