55721972620a81a2543e5c7749c2aaa7b1fc351b
Karl Heitmann <khf1988@gmail.com>
2020-11-01 16:56:43 -0300

    formulario funciona

--- app/controllers/nasa_photos_controller.rb
+++ app/controllers/nasa_photos_controller.rb
@@ -8,6 +8,17 @@ class NasaPhotosController < ApplicationController
     # puts resp
     puts @data
 =end
+    if (params.has_key?(:q))
+      url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY'
+      uri = URI(url)
+      resp = Net::HTTP.get(uri) # => String
+      data = JSON.parse(resp)
+      # puts resp
+      @photos = data['photos']
+      # puts @photos
+    else
+      @photos = []
+    end
 
   end
 end
