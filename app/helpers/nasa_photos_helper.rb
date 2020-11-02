fccb49a11abc59e0bf6e0fc33f975b902ef8260c
Karl Heitmann <khf1988@gmail.com>
2020-11-02 02:49:08 -0300

    corregido build_web_page para generar el codigo como lo pide el documento

--- app/helpers/nasa_photos_helper.rb
+++ app/helpers/nasa_photos_helper.rb
@@ -1,10 +1,12 @@
 module NasaPhotosHelper
   def build_web_page(photos)
-    photos.collect do |photo|
-      content_tag(:div, class: "col-md-3") do
-        tag("img", src: photo['img_src'])
-      end
-    end.join.html_safe
+    content_tag(:ul) do
+      photos.collect do |photo|
+        content_tag(:li) do
+          tag("img", src: photo['img_src'])
+        end
+      end.join.html_safe
+    end
   end
 
 end
